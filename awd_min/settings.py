66de656d290a44a846010e04d29ff5eab8e3eae2
abdullah <apple@apples-MacBook-Air.local>
2024-06-17 13:36:46 +0500

    Emial Configuration and python decouple use for scrate data not send to git

--- awd_min/settings.py
+++ awd_min/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r=&hvk*@=iystodd0(7hxf&_+zad%u2dqi_!05pv!c&r5z&0mk"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool) # True or false
 
 ALLOWED_HOSTS = []
 
@@ -136,3 +137,11 @@ MESSAGE_TAGS = {
 }
 
 CELERY_BROKER_URL = 'redis://localhost:6379'
+
+# Email Configuration 
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True 
+DEFAULT_FROM_EMAIL = 'Automate with Django <aj161474@gmail.com>'
