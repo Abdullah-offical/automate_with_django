0b31ad04aec0af20f2459b056c0e7939b71af3a3
abdullah <apple@apples-MacBook-Air.local>
2024-06-22 22:06:15 +0500

    Login & Register

--- awd_min/settings.py
+++ awd_min/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'dataentry',
     'uploads',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -150,3 +152,6 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True 
 DEFAULT_FROM_EMAIL = 'Automate with Django <aj161474@gmail.com>'
 DEFAULT_TO_EMAIL = 'abdullahoffical821@gmail.com'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
--- awd_min/views.py
+++ awd_min/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from dataentry.tasks import celery_test_task
+from .forms import RegistrationForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm 
+from django.contrib import auth
+from django.contrib.auth import logout
+
 
 
 def home(request):
@@ -10,3 +16,52 @@ def celery_test(request):
     # I want to exectute a time consuming task here
     celery_test_task.delay()
     return HttpResponse("<h3>Function exsuted succesfully</h3>")
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration Successful')
+            return redirect('register')
+        else:
+            context = {
+                'form': form
+            }
+            return render(request, 'register.html', context)
+    else:
+        form = RegistrationForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'register.html', context)
+    
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = auth.authenticate(username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Invalid username or password')
+        else:
+            messages.error(request, 'Invalid username or password')
+        return redirect('login')
+    else:
+        form = AuthenticationForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'login.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
