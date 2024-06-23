72953f8c323e6dff3d0eed9dcee0ff95c4d72b62
abdullah <apple@apples-MacBook-Air.local>
2024-06-23 12:29:55 +0500

    Save email and show message Video #64

--- emails/views.py
+++ emails/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import EmailForm
+from django.contrib import messages
 
 
 def email_send(request):
     if request.method == 'POST':
-        return
+        email_form = EmailForm(request.POST, request.FILES)
+        if email_form.is_valid():
+            email_form.save()
+            # send email
+
+
+            # Display a success message
+            messages.success(request, 'Email Send Successfully!')
+            return redirect('email_send')
     else:
         email_form = EmailForm()
         context = {
