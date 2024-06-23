6f54af131a50f9933d8f5db54dc22d8d8732c845
abdullah <apple@apples-MacBook-Air.local>
2024-06-23 14:09:33 +0500

    Extract email address and send email to all with out attachment

--- emails/views.py
+++ emails/views.py
@@ -3,17 +3,35 @@ from .forms import EmailForm
 from django.contrib import messages
 from dataentry.utils import send_email_notification
 from django.conf import settings
+from .models import Subscriber
 
 
 def email_send(request):
     if request.method == 'POST':
         email_form = EmailForm(request.POST, request.FILES)
         if email_form.is_valid():
-            email_form.save()
+            email_form = email_form.save()
             # send email
             mail_subject = request.POST.get('subject')
             message = request.POST.get('body')
-            to_email = settings.DEFAULT_TO_EMAIL
+            email_list = request.POST.get('email_list')
+            
+            # Access the selected email list
+            email_list = email_form.email_list
+            # print(email_list)
+
+            # Extract email addresses from the subscribe model in the selected email list
+            subscribers = Subscriber.objects.filter(email_list=email_list)
+
+
+            to_email = [email.email_address for email in subscribers] # list compresion below for loop
+            # to_email = []
+            # for email in subscribers:
+            #     # email address comes from scribe model fiels 
+            #     to_email.append(email.email_address)
+            # print("======> ",to_email)
+            
+
             send_email_notification(mail_subject, message, to_email)
 
 
