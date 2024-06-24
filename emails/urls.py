c9026dc0fad95f0ea095a2f1a82f096b2afb8f6c
abdullah <apple@apples-MacBook-Air.local>
2024-06-24 10:31:31 +0500

    send frontend & and fatch the data

--- emails/urls.py
+++ emails/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('email-send/', views.email_send, name='email_send'),
+    path('track/click/<unique_id>', views.track_click, name='track_click'),
+    path('track/open/<unique_id>', views.track_open, name='track_open'),
+    path('track/dashboard', views.track_dashboard, name='track_dashboard'),
 ]
--- emails/views.py
+++ emails/views.py
@@ -5,7 +5,7 @@ from .forms import EmailForm
 from django.contrib import messages
 from dataentry.utils import send_email_notification
 from django.conf import settings
-from .models import Subscriber
+from .models import Email, Subscriber
 
 
 def email_send(request):
@@ -59,3 +59,17 @@ def email_send(request):
             'email_form' : email_form
         }
     return render(request, 'emails/email-send.html', context)
+
+
+def track_click(request):
+    return 
+
+def track_open(request):
+    return
+
+def track_dashboard(request):
+    emails = Email.objects.all()
+    context = {
+        'emails' : emails
+    }
+    return render(request, 'emails/track_dashboard.html', context)
