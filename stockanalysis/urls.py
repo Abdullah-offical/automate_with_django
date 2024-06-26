54d52a10f311769a20931642438f9572f4239dca
abdullah <apple@apples-MacBook-Air.local>
2024-06-26 14:14:54 +0500

    Autocomplete searching setup

--- stockanalysis/urls.py
+++ stockanalysis/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('stock/', views.stocks, name='stocks'),
+    path('stock-autocomplete/', views.StockAutocomplete.as_view(), name='stock_autocomplete'),
 ]
--- stockanalysis/views.py
+++ stockanalysis/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from dal import autocomplete
+from .models import Stock
+from .forms import StockForm
 
 def stocks(request):
-    return render(request, 'stockanalysis/stocks.html')
+    if request.method =='POST':
+        return
+    else:
+        form = StockForm
+        context = {
+            'form' : form,
+        }
+    return render(request, 'stockanalysis/stocks.html', context)
+
+
+class StockAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        qs = Stock.objects.all()
+
+        if self.q:
+            print('entered keyword=>', self.q)
+            qs = qs.filter(name__istartswith=self.q)
+            print('result==>', qs)
+        return qs
