a4269a47c7720225f6df4702816142d3b6f26870
abdullah <apple@apples-MacBook-Air.local>
2024-06-29 11:34:13 +0500

    logical all setup for image compression

--- image_compression/views.py
+++ image_compression/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CompressImageForm
+from PIL import Image
+import io
+from django.http import HttpResponse
 
 def compress(request):
+    user = request.user
     if request.method == 'POST':
-        return
+        form = CompressImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            original_img = form.cleaned_data['original_img']
+            quality = form.cleaned_data['quality']
+
+            compressed_image = form.save(commit=False)  # comit false is tempery data save
+            compressed_image.user = user 
+
+            # perform compression
+            img = Image.open(original_img)
+
+            output_format = img.format #set format of the image save farmat
+            buffer = io.BytesIO()  # image change to bytes
+            img.save(buffer, format=output_format, quality=quality) # complete image save
+            buffer.seek(0)
+
+            # save the compressed image inside the model
+            compressed_image.compressed_img.save(
+                f'compressed_{original_img}', buffer
+            )
+
+            return redirect('compress')
     else:
         form = CompressImageForm()
         context = {
