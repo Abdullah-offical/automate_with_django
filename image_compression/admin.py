e8ac4e2a16f9bb6282f8f43642c87cc248af14c0
abdullah <apple@apples-MacBook-Air.local>
2024-06-29 11:44:50 +0500

    Display image thumbnail on admin site, with custom create thumbnail colum

--- image_compression/admin.py
+++ image_compression/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import CompressImage
+from django.utils.html import format_html
 
-admin.site.register(CompressImage)
+class CompressImageAdmin(admin.ModelAdmin):
+    def thumbnail(self, obj):
+        return format_html(f'<img src="{obj.compressed_img.url}" width="40" height="40">')
+    list_display = ('user', 'thumbnail')
+    
+admin.site.register(CompressImage, CompressImageAdmin)
