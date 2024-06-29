9845a407275b7117c90bc6858a385be795ab079c
abdullah <apple@apples-MacBook-Air.local>
2024-06-29 11:19:13 +0500

    Form setup in frontend

--- image_compression/views.py
+++ image_compression/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .forms import CompressImageForm
 
 def compress(request):
     if request.method == 'POST':
         return
     else:
-        return render(request, 'image_compress/compress.html')
+        form = CompressImageForm()
+        context = {
+            'form' : form
+        }
+        return render(request, 'image_compress/compress.html', context)
